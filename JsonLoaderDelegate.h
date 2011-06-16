af99ac454a53066434e63773f4764ec35cce32bd
Traun Leyden <tleyden@branchcut.com>
2011-06-16 12:35:43 -0700

    License + README

--- JsonLoaderDelegate.h
+++ JsonLoaderDelegate.h
@@ -1,5 +1,19 @@
 
-
+/*
+ ``The contents of this file are subject to the Mozilla Public License
+ Version 1.1 (the "License"); you may not use this file except in
+ compliance with the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/
+ 
+ The Initial Developer of the Original Code is Hollrr, LLC.
+ Portions created by the Initial Developer are Copyright (C) 2011
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s):
+ 
+ Dustin Dettmer <dusty@dustytech.com>
+ 
+ */
 
 @class JsonLoader;
 
